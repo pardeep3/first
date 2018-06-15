cf6f5869bfdda458cccc0c0e1c6cd14a54fd8210
pardeep3 <40191042+pardeep3@users.noreply.github.com>
2018-06-15 00:51:41 -0700

    assgmnt2

---
+++ assigment2.py
@@ -0,0 +1,20 @@
+#question1
+print("hello")
+#question2
+print("acad"+"view")
+#question3
+x=input("enter number")
+y=input("enter name")
+z=input("enter class")
+print(x,y,z)
+#question4
+print("let\'s get started on screen")
+#question5
+s="acadview"
+course="python"
+fees=5000
+print("weclome to %s for %s course and fee is %d" %(s,course,fees))
+#question6
+name="tony shark"
+salary=10000
+print("%s %d" %(name,salary))
