ec95684410810aa2be6faa942d63f86947a2fd15
pardeep3 <40191042+pardeep3@users.noreply.github.com>
2018-06-18 05:46:11 -0400

    assignmnt4

---
+++ assignment4.py
@@ -0,0 +1,49 @@
+'''#ques 1
+t=(1,2,3)
+print(t)
+print(len(t))
+
+
+#ques 2
+t=(1,2,3,4,5,6,7,8,9)
+print(t)
+print(max(t))
+print(min(t))
+
+
+# ques 3
+t=(1*2*3*3*9)
+print(t)
+
+#ques 4
+
+a=set([1,2,3,4])
+b=set([4,3,6,5])
+print(a)
+print(b)
+print(a - b)
+print(a & b)
+print(a<=b)
+print(a>=b)
+
+
+#ques 5
+d={"name":"deepali", "marks":[90,100,56]}
+print(d)
+'''
+
+#ques 6
+d={"name":"deepali","marks":[90,100,56]}
+print(sorted(d["marks"]))
+
+'''# ques 7
+
+d=["m","i","s","s","i","s","s","i","p","p","i"]
+print(d)
+m=d.count("m")
+i=d.count("i")
+s=d.count("s")
+p=d.count("p")
+l={'m':m,'i':i,'s':s,'p':p}
+print(l)
+'''
