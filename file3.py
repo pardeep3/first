6d7f636fa50ce180e461e911f800e9e694484cb7
pardeep3 <40191042+pardeep3@users.noreply.github.com>
2018-06-18 06:36:32 -0400

    assig3

---
+++ file3.py
@@ -0,0 +1,37 @@
+#ques 1
+
+a=input("enter no.")
+l=[]
+l.append(a)
+print(l)
+
+#ques 2
+
+b=["google","apple","facebook","microsoft","tesla"]
+b.append(l)
+print(b)
+
+#ques 3
+
+a=[1,2,3,12,21,34,2]
+print(a.count(2))
+
+#ques 4
+
+a=[8,67,89,1]
+a.sort()
+print(a)
+
+#ques 5
+
+a=[1,2,3]
+b=[4,5,6]
+print(a+b)
+
+#ques 6
+
+a=[1,2,3,4]
+a.pop()
+print(a)
+a.append(6)
+print(a)
